3e0e1b374ed3fcd8d3d977137377a2fbe5629d6c
JustCallDinesh <dinesh7091566641@gmail.com>
2026-09-16 22:21:27 +0530

    second  coomit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProdectedRoute";
 import Layout from "./components/Layout";
 
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/Loginpage";
 import DashboardPage from "./pages/DashboardPage";
 import StudentsPage from "./pages/StudentsPage";
 import CoursesPage from "./pages/CoursePage";
